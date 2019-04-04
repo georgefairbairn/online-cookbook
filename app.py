6ab14c3c4194e850c894fd5eb4e014e6f6012477
George Fairbairn <george.fair@icloud.com>
2019-04-04 11:46:36 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask import Flask
+
+app = Flask(__name__)
+# app.config["MONGO_DBNAME"] = 'online_cookbook'
+# app.config["MONGO_URI"] = 'mongodb://admin:admin@ds123926.mlab.com:23926/task_manager'
+
+# mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
