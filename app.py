ac6e927e955f43677a8652394269b0456deebb23
George Fairbairn <george.fair@icloud.com>
2019-04-04 13:18:20 +0000

    Added materialize components to project

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+online_cookbook_pw = 'r00tUser'
+connection_string = 'mongodb+srv://root:' + online_cookbook_pw + '@myfirstcluster-lefha.mongodb.net/online_cookbook?retryWrites=true'
 
 app = Flask(__name__)
-# app.config["MONGO_DBNAME"] = 'online_cookbook'
-# app.config["MONGO_URI"] = 'mongodb://admin:admin@ds123926.mlab.com:23926/task_manager'
+app.config["MONGO_DBNAME"] = 'online_cookbook'
+app.config["MONGO_URI"] = connection_string
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+    
 
 
 if __name__ == '__main__':
